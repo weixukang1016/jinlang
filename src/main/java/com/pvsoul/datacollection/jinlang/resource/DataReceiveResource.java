55301c39ad0fb290644b0e79840ae4d479dba304
wei_health <78266110@qq.com>
2021-01-06 11:22:16 +0800

    打出请求头信息日志

--- src/main/java/com/pvsoul/datacollection/jinlang/resource/DataReceiveResource.java
+++ src/main/java/com/pvsoul/datacollection/jinlang/resource/DataReceiveResource.java
@@ -7,12 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Enumeration;
 
 //@Api(value = "DataReceiveResource")
 @Path("api/datareceive")
@@ -27,9 +30,13 @@ public class DataReceiveResource {
 
     @POST
     @Path("/pushdata")
-    //@ApiOperation("实时轨迹点推送")
-    public Response pushData(JinLangDataDao data) {
-
+    //@ApiOperation("接收锦浪云平台推送数据")
+    public Response pushData(@Context HttpServletRequest request, JinLangDataDao data) {
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            log.info(headerName + ":" + request.getHeader(headerName));
+        }
         log.info(data.toString());
         ResultDao resultDao = dataReceiveService.SaveData(data);
         return Response.status(Response.Status.OK).entity(resultDao).build();
